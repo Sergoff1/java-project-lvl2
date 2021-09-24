2dec0e59bf8bc418789c9c93301c2c590366c8eb
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-09-24 21:34:20 +0300

    Add picocli

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.3",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
